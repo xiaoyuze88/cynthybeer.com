ce3f22cf0eab5b45b0455d81ccaaf14a1f8632ad
xyz <xiaoyuze88@gmail.com>
2016-06-16 12:25:08 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Hi, cynthybeer' });
+  res.render('index', { title: 'Hi, dear Cynthy' });
 });
 
 module.exports = router;
